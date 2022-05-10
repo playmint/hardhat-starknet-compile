a53c0e92d09ac63d4778ee66012829a0fd71addd
Anders-T | 0x80.eth <anders.torbjornsen87@gmail.com>
2022-05-10 14:15:00 +0100

    writing a cairo files cache file, not reading it yet though

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { subtask, task, types } from "hardhat/config";
+import { HardhatPluginError } from "hardhat/plugins";
 import { TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
 import fs from "fs";
-import { exec } from "child_process";
+import { exec, ExecException } from "child_process";
 import path from "path"
+import { createHash } from "crypto";
 
 export const TASK_STARKNET_COMPILE: string = "starknet-compile";
 export const TASK_STARKNET_COMPILE_GATHER_CAIRO_FILES: string = "starknet-compile:gather-cairo-files";
@@ -21,9 +23,7 @@ task(TASK_COMPILE)
 task(TASK_STARKNET_COMPILE)
     .setAction(async (args, hre) => {
         const cairoFiles: string[] = await hre.run(TASK_STARKNET_COMPILE_GATHER_CAIRO_FILES);
-        console.log(cairoFiles);
         const toCompile: string[] = await hre.run(TASK_STARKNET_COMPILE_GET_FILES_TO_COMPILE, { sources: cairoFiles });
-        console.log(toCompile);
         await hre.run(TASK_STARKNET_COMPILE_COMPILE, { sources: toCompile });
     });
 
@@ -63,7 +63,6 @@ subtask(TASK_STARKNET_COMPILE_GATHER_CAIRO_FILES)
 subtask(TASK_STARKNET_COMPILE_GET_FILES_TO_COMPILE)
     .addParam("sources", undefined, undefined, types.any)
     .setAction(async (args: { sources: string[] }): Promise<string[]> => {
-        console.log(args);
         return args.sources;
     });
 
@@ -75,6 +74,8 @@ subtask(TASK_STARKNET_COMPILE_COMPILE)
         // TODO configurable
         const artifactsDir = "artifacts-starknet";
 
+        let cache: { [file: string]: any } = {}; // TODO need to pass this in from parent task so it can written to disk regardless of exceptions
+
         for (const source of args.sources) {
             // TODO configurable artifacts dir
             console.log(source);
@@ -84,15 +85,58 @@ subtask(TASK_STARKNET_COMPILE_COMPILE)
                 fs.mkdirSync(outDir, { recursive: true });
             }
 
-            await new Promise((resolve, reject) => {
-                exec(`starknet-compile "${source}" --output "${outFile}"`, (error, stdout) => {
-                    if (error) {
-                        reject(error);
-                    }
-                    else {
-                        resolve(stdout);
-                    }
+            try {
+                await new Promise((resolve, reject) => {
+                    exec(`starknet-compile "${source}" --output "${outFile}" --cairo_dependencies deps.txt`, (error, stdout) => {
+                        if (error) {
+                            reject(error);
+                        }
+                        else {
+                            resolve(stdout);
+                        }
+                    });
                 });
-            });
+            }
+            catch (error) {
+                const exc = (error as ExecException);
+
+                // remove the "command failed etc" line, we already know that
+                let msgRows = exc.message.split("\n");
+                if (msgRows[0].startsWith("Command failed: starknet-compile ")) {
+                    msgRows = msgRows.slice(1);
+                }
+                throw new HardhatPluginError("hardhat-starknet-compile", "compilation of cairo contracts failed: \n" + msgRows.join("\n"));
+            }
+
+            let deps: { [file: string]: any } = {};
+            const depsRows = fs.readFileSync("deps.txt").toString().split("\n");
+            fs.rmSync("deps.txt");
+
+            // note, these deps include the current file, which is why we don't 
+            // explicitly hash it
+            for (let i = 0; i < depsRows.length; ++i) {
+                const depsRow = depsRows[i].trim();
+
+                // ignore any empty row
+                // ignore the SET (DEPENDENCIES line at the start, and the closing )
+                if (depsRow.length == 0 ||
+                    ["SET (DEPENDENCIES", ")"].indexOf(depsRow) != -1) {
+                    continue;
+                }
+
+                const stats = fs.statSync(depsRows[i]);
+                const hash = createHash("md5").update(fs.readFileSync(depsRows[i])).digest().toString("hex");
+                deps[depsRows[i]] = {
+                    lastModificationTime: stats.mtime.getTime(),
+                    hash: hash
+                }
+            }
+
+            cache[source] = {
+                dependencies: deps
+            };
         }
+
+        // TODO is the cache dir folder configurable?
+        fs.writeFileSync("./cache/cairo-files-cache.json", JSON.stringify(cache, null, 4));
     });
